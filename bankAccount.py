ccc433370bbc07bbdf6c15801cd08b7494f7d803
Peter <creanp2@gmail.com>
2025-02-13 13:07:51 -0500

    feat: bankaccount class to be inherited

---
+++ bankAccount.py
@@ -0,0 +1,50 @@
+
+class BankAccount:
+
+    def __init__(self, name, curr_bal, mini_bal):
+
+        self.title = "Turbo Credit Union"
+
+        self.name = name
+        self.curr_bal = curr_bal
+        self.mini_bal = mini_bal
+
+    def deposit(self, value):
+        self.curr_bal += value
+
+    def withdraw(self, value):
+        if self.curr_bal - value < self.mini_bal:
+            print('Unable to withdraw. Requested amount exceeds available funds.')
+        else:
+            self.curr_bal -= value
+
+    def print_customer_information(self):
+        printstr = self.title + """ Account:
+        --------------------------
+        # Account Owner: """ + self.name + """
+        Current Balance: """ + str(self.curr_bal)
+
+        print(printstr)
+
+        # Turbo Credit Union Account:
+        # --------------------------
+        # Account Owner: name
+        # Current Balance: curr_bal
+
+
+
+print("Testing the deposit function:")
+acct1 = bankAccount('David', 100, 0)
+acct2 = bankAccount('Ann', 100, 0)
+
+acct1.print_customer_information()
+acct1.deposit(200)
+acct1.print_customer_information()
+
+print("Testing the withdraw function:")
+acct1.withdraw(100)
+acct1.print_customer_information()
+acct2.print_customer_information()
+acct2.withdraw(200)
+
+# Assignment completed alone by Peter Crean
