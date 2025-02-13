f6767e6563ed5b174dd0a47a9f9fbd3eb269c255
QueenSophiaLo <75154780+QueenSophiaLo@users.noreply.github.com>
2025-02-13 18:15:24 -0500

    feat: Adding error checking to withdraw feature

--- bankAccount.py
+++ bankAccount.py
@@ -7,14 +7,17 @@ from savings_and_checking import SavingsAccount
 
 class BankAccount:
 
-    def __init__(self, name, curr_bal, mini_bal):
-
+        # Class Attribute: Title of the bank
         self.title = "Turbo Credit Union"
+
+    # Instance Attributes: name (customer name), curr_bal (current balance), mini_bal (minimum balance)
+    def __init__(self, name, curr_bal, mini_bal):
 
         self.name = name
         self.curr_bal = curr_bal
         self.mini_bal = mini_bal
 
+    # Method 1: deposit money
     def deposit(self, value):
         print(f"Current amount is {self.curr_bal}")
         
@@ -23,12 +26,19 @@ class BankAccount:
             print(f"You deposited {value}. Your new balance is {self.curr_bal}.")
         else:
             print("You deposited an invalid amount")
-
+ 
+    # Method 2: withdraw money
     def withdraw(self, value):
-        if self.curr_bal - value < self.mini_bal:
-            print('Unable to withdraw. Requested amount exceeds available funds.')
-        else:
+        print(f"Current amount is {self.curr_bal}")
+
+        # Adding Validation: If remaining balance is less than minimal balance, user can't withdraw
+        if value > 0 and self.curr_bal - amount >= self.mini_bal:
             self.curr_bal -= value
+            print(f"You withdrew {value}. Your new balance is {self.curr_bal}.")
+        elif amount > 0:
+            print(f"Insufficient funds. Remaining balance must be at least {self.mini_bal}.")
+        else:
+            print("Withdrawal amount must be positive.")
 
     def print_customer_information(self):
         printstr = self.title + """ Account:
