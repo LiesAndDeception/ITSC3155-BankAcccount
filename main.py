35cb5c64dc383dfbee00e08af84c137d391e2f54
QueenSophiaLo <75154780+QueenSophiaLo@users.noreply.github.com>
2025-02-13 19:17:15 -0500

    feat: importing modules and BankAccount class

---
+++ main.py
@@ -0,0 +1,9 @@
+# Author: Sophia Godfrey (TODO add Peter when merged in final branch)
+# Class: Intro to Software Engineering (ITSC 3155)
+# Date: February 13, 2025
+# Assignment: Bank Account Part 2
+
+from bankAccount import BankAccount
+from savings_account import SavingsAccount
+from checking_account import CheckingAccount
+
